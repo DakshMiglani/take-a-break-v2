9e7724c2fe1135f9e23bfecb37dfd04ab9344156
Daksh Miglani <dakshm@thegamersverge.net>
2016-09-14 22:40:08 +0530

    fun for atman

--- takebreak.py
+++ takebreak.py
@@ -1,6 +1,10 @@
 import webbrowser
 import time
-time.sleep(60)
-webbrowser.open_new("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+totalBreaks = 10
+takenBreaks = 0
 
-
+while(takenBreaks < totalBreaks):
+    time.sleep(60*60*2)
+    webbrowser.open_new("https://www.youtube.com/watch?v=9_IWRuryzaY")
+    takenBreaks + 1
+    print("The link was opened on " + time.ctime() + "This program was made of A Shah.")
