5c911b36a664b2d3102f9334264d3c423fe31dbe
Daksh Miglani <dakshm@thegamersverge.net>
2016-09-15 12:58:33 +0530

    Every 2 hours, it would play the link of a video

--- takebreak.py
+++ takebreak.py
@@ -1,10 +1,10 @@
 import webbrowser
 import time
-totalBreaks = 10
+totalBreaks = 3
 takenBreaks = 0
 
 while(takenBreaks < totalBreaks):
     time.sleep(60*60*2)
-    webbrowser.open_new("https://www.youtube.com/watch?v=9_IWRuryzaY")
+    webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     takenBreaks + 1
     print("The link was opened on " + time.ctime() + "This program was made of A Shah.")
