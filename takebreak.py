004c71749ae13f4922e668f19afb737425cde22c
Daksh Miglani <dakshm@thegamersverge.net>
2016-09-15 13:06:02 +0530

    fixed an error and added a completion message

--- takebreak.py
+++ takebreak.py
@@ -4,7 +4,11 @@ totalBreaks = 3
 takenBreaks = 0
 
 while(takenBreaks < totalBreaks):
-    time.sleep(60*60*2)
+    takenBreaks = takenBreaks + 1
+    time.sleep(3)
     webbrowser.open_new("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    takenBreaks + 1
-    print("The link was opened on " + time.ctime() + "This program was made of A Shah.")
+
+
+print("The link was opened on " + time.ctime() + "This program was made of A Shah.")
+if(takenBreaks == totalBreaks):
+        print("The Program has finished the work.")
